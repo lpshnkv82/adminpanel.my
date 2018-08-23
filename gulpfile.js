94ab0a9ba3f422b8a82c8df9f53a2df0f600b80f
ldrv565 <40745264+ldrv565@users.noreply.github.com>
2018-08-23 16:07:16 +0300

    Верстка pageMaker.html под настоящую версию админ панели.
    Ускорение верстки.
    Верстка pageMaker.html под настоящую версию админ панели для того, чтобы можно было работать со стилями через SASS.
    Теперь я могу отслеживать внешние изменения CSS и править SASS-код под эти изменения.

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp'), // Подключаем Gulp
+    sass = require('gulp-sass'), //Подключаем Sass пакет,
+    browserSync = require('browser-sync'); // Подключаем Browser Sync
+
+gulp.task('sass', function() { // Создаем таск Sass
+    return gulp.src('public_html/app/sass/**/*.sass') // Берем источник
+        .pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
+        .pipe(gulp.dest('public_html/template/admin/css')) // Выгружаем результата в папку app/css
+        .pipe(browserSync.reload({ stream: true })) // Обновляем CSS на странице при изменении
+});
+
+gulp.task('browser-sync', function() { // Создаем таск browser-sync
+    browserSync({ // Выполняем browserSync
+        server: { // Определяем параметры сервера
+            baseDir: 'public_html' // Директория для сервера - app
+        },
+        notify: false // Отключаем уведомления
+    });
+});
+
+gulp.task('watch', ['browser-sync', 'sass'], function() {
+    gulp.watch('public_html//sass/**/*.sass', ['sass']); // Наблюдение за sass файлами в папке sass
+    gulp.watch('public_html//*.html', browserSync.reload); // Наблюдение за HTML файлами в корне проекта
+    gulp.watch('public_html//js/**/*.js', browserSync.reload); // Наблюдение за JS файлами в папке js
+});
+
+gulp.task('default', ['watch']);
