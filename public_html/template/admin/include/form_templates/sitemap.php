444b35a017ab4e38c2fffa0efe18122244c6df66
ldrv565 <40745264+ldrv565@users.noreply.github.com>
2018-09-26 17:02:29 +0300

    Кнопка sitemap

---
+++ public_html/template/admin/include/form_templates/sitemap.php
@@ -0,0 +1,16 @@
+<!--/Блок с кнопкой-->
+<div class="vg-element vg-full vg-box-shadow">
+    <div class="vg-wrap vg-element vg-full vg-box-shadow">
+            <div class="vg-element vg-full vg-left">
+                <span class="vg-header">Sitemap</span>
+            </div>
+            <!--кнопка-->
+            <div class="vg-element vg-fourth vg-left">
+                <a href="#" class="vg-text vg-firm-color1 vg-firm-background-color4 vg-input vg-button">
+                    Создать sitemap
+                </a>
+            </div>
+            <!--/кнопка-->
+    </div>
+</div>
+<!--/Блок с кнопкой-->
