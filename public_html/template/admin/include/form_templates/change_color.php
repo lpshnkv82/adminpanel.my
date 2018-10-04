dd49a035d65ab409285f16b54d580ab4e8cd6fe3
ldrv565 <40745264+ldrv565@users.noreply.github.com>
2018-10-04 12:23:10 +0300

    Блок изменения цвета и модальное окно с палитрой

---
+++ public_html/template/admin/include/form_templates/change_color.php
@@ -0,0 +1,29 @@
+<div class="vg-element vg-full vg-box-shadow">
+    <div class="vg-wrap vg-element vg-full vg-box-shadow">
+        <div class="vg-wrap vg-element vg-full">
+            <div class="vg-element vg-full vg-left">
+                <span class="vg-header">Изменение цвета</span>
+            </div>
+            <div class="vg-element vg-full vg-left">
+                <span class="vg-text vg-firm-color5">Выберите цвет или в ведите в формате rgb. Пример: 200, 100, 0</span>
+            </div>
+        </div>
+        <div class="vg-element vg-full">
+            <div class="vg-element">
+                <input type="text" name="<?=$row?>" class="vg-input vg-text vg-firm-color1" value="red, green, blue">
+            </div>
+            <div class="vg-element">
+                <div class="vg-dotted-square vg-dotted-square--small">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="modal vg-box-shadow vg-firm-background-color4">
+    <div class="palette vg-box-shadow vg-firm-background-color3">
+
+    </div>
+    <span class="vg-element vg-input vg-button vg-text">
+        Выбрать
+</span>
+</div>
